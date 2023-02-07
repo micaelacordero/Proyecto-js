4d5d2a637e16f3ccdce2f6b344b1c38d3b7b4350
Micaela Cordero <micaela.cordero94@gmail.com>
2023-02-07 19:55:58 -0300

    cambios

--- js/script.js
+++ js/script.js
@@ -1,113 +1,57 @@
-
-
-
-function Producto (nombre, precio, stock) {
+function Producto (nombre, precio, stock, img) {
   this.nombre = nombre;
   this.precio = precio;
   this.stock = stock || 0;
+  this.img = img
+  this.restarStock = function(cantidad){
+    this.stock -= cantidad
+  }
+  {
+    this.sumarStock = function(cantidad){
+      this.stock += cantidad
+    }
+  }
 }
 
-let producto1 = new Producto ("bacha artesanal", 4850, 5)
+let producto1 = new Producto ("bacha artesanal", 4850, 5, "../media/BachaArtesanal.jpeg")
 
-let producto2 = new Producto ("jarrita acqua", 1200, 20)
+let producto2 = new Producto ("jarrita acqua", 1200, 20,"../media/Jarrita Acqua.jpeg")
 
-let producto3 = new Producto ("jarrón rubí", 3300, 25)
+let producto3 = new Producto ("jarrón rubí", 3300, 25, "../media/JarrónRubí.jpeg")
 
-let producto4 = new Producto ("cuenco acqua", 1300, 30)
+let producto4 = new Producto ("cuenco acqua", 1300, 30, "../media/CuencoAcqua.jpeg")
 
-let producto5 = new Producto ("ensaladera mar", 2800, 20)
+let producto5 = new Producto ("taza rústica rubí", 1500, 50, "../media/TazarústicaRubi.jpeg")
+
+let producto6 = new Producto ("ensaladera mar", 2800, 20, "../media/EnsaladeraMar.jpeg")
+
+
 
 let precioTotal = 0
 
-let listaProductos = [producto1 , producto2, producto3, producto4, producto5] 
+/*Cards*/
 
-let listaNombres = []
+let productos = document.getElementById("productos")
 
+for (const prod of productos){
 
-for (const producto of listaProductos){
-  listaNombres.push(producto.nombre) 
+let card = document.createElement("div")
+
+card.className = "card"
+
+card.innerHTML = `<img src="${prod.img}" height="200" width="400"></imagen><h1>${prod.nombre}</h1><p>Precio: $${prod.precio}</p><button id="boton"${prod.listaProductos}>Comprar</button>`
+
+productos.append(card)
 }
 
-function saludo (){
-  alert ("Gracias por ingresar a Magia Blanca!")
+let boton = document.getElementById("boton")
+
+boton.addEventListener("click", mostrarPorConsola)
+
+function mostrarPorConsola(){
+  console.log("click")
 }
 
-saludo();
+let input = document.getElementById("search")
 
-alert ("Aquí podra adquirir los siguientes productos: \n - " + listaProductos.join("\n - "))
-  
-
-let cantidadCompra = prompt ("Cuantos productos desea adquirir?")
-
-for(let i = 0; i < cantidadCompra; i = i + 1){
-  let productoCompra = prompt("Que producto desea comprar?: \n - Bacha Artesanal \n - Jarrita Acqua \n - Jarrón Rubí \n - Cuenco Acqua \n - Ensaladera Mar \n - Salir"  )
-
-if (productoCompra == "Salir"){
-   break
-}
-
-if (productoCompra == Producto){
-
-  let cantidadProducto = prompt("Ingrese que cantidad de " + Producto + " desea comprar:")
-  if (cantidadProducto <= Producto.stock){
-       precioTotal = precioTotal + (cantidadProducto * Producto.precio)
-  }
-  else {
-      alert("El stock actual de este producto es de: " + Producto.stock + ". En caso de necesitar realizar un pedido mayorista por favor comunicarse a través de la página de contacto.")
-  }
-}
-
-else if (productoCompra == Producto){
-
-  let cantidadProducto = prompt("Ingrese que cantidad de " + Producto + " desea comprar:")
-  if (cantidadProducto <= Producto.stock){
-       precioTotal = precioTotal + (cantidadProducto * Producto.precio) 
-  }
-  else {
-    alert("El stock actual de este producto es de: " + Producto.stock + ". En caso de necesitar realizar un pedido mayorista por favor comunicarse a través de la página de contacto.")
-}
-}
-
-/*else if (productoCompra == "jarrón rubí"){
-
-  let cantidadProductoJarrón = prompt("Ingrese que cantidad de " + producto3.nombre + " desea comprar:")
-  if (cantidadProductoJarrón <= producto3.stock) {
-       precioTotal = precioTotal + (cantidadProductoJarrón * producto3.precio) 
-  }
-  else {
-    alert("El stock actual de este producto es de: " + producto3.stock + ". En caso de necesitar realizar un pedido mayorista por favor comunicarse a través de la página de contacto.")
-} 
-}
-
-else if (productoCompra == "cuenco acqua"){
-
-  let cantidadProductoCuenco = prompt("Ingrese que cantidad de " + producto4.nombre + " desea comprar:")
-  if (cantidadProductoCuenco <= producto4.stock) {
-        precioTotal = precioTotal + (cantidadProductoCuenco * producto4.precio) 
-  }
-  else {
-    alert("El stock actual de este producto es de: " + producto4.stock + ". En caso de necesitar realizar un pedido mayorista por favor comunicarse a través de la página de contacto.")
-}  
-}
-
-else if (productoCompra == "ensaladera mar"){
-
-  let cantidadProductoEnsaladera = prompt("Ingrese que cantidad de " + producto5.nombre + " desea comprar:")
-  if (cantidadProductoEnsaladera <= producto5.stock) {
-        precioTotal = precioTotal + (cantidadProductoEnsaladera * producto5.precio) 
-  }
-  else {
-    alert("El stock actual de este producto es de: " + producto5.stock + ". En caso de necesitar realizar un pedido mayorista por favor comunicarse a través de la página de contacto.")
-}
-}*/
-} 
-
-if (precioTotal != 0){
-  alert("El precio total es: " + precioTotal)
-}
-
-function saludo (){
-  alert ("Gracias por ingresar a Magia Blanca!")
-}
-
-saludo();
+input.addEventListener('input', () => {console.log(input.value)})
